9e350c14200717770318506dbe71036d1d952120
metairie <metairie@ebu.ch>
2017-12-27 11:25:31 +0100

    put test files + check filename and untar

--- ebusrt-listener/scripts/listener.sh
+++ ebusrt-listener/scripts/listener.sh
@@ -49,6 +49,47 @@ do
 	if [ $? -eq 0 ]
 	then
 		echo " Success "
+		IN=${1:-0.0.0.0-20000101-000000-000000.tar}
+
+		OIFS=$IFS
+		IFS='-'
+		m=$IN
+		count=0
+		IP="255.255.255.255"
+		DATEYMD="19991231"
+		TIMEHMS="235959"
+		RND="999999.tar"
+		for x in $m
+		do
+			# IP
+			if [ $count -eq 0 ]; then
+				IP=$x
+			fi
+
+			# date YYMMDD
+			if [ $count -eq 1 ]; then
+				DATEYMD=$x
+			fi
+			
+			# date time HHMMSS
+			if [ $count -eq 2 ]; then
+				TIMEHMS=$x
+			fi
+			
+			# random
+			if [ $count -eq 3 ]; then
+				RND=$x
+			fi
+			
+			((count++))
+		done
+		IFS=$OIFS
+		FILENAME="$IP-$DATEYMD-$TIMEHMS-$RND"
+		echo "$DATEYMD:$TIMEHMS [$IP] successfully received file name $FILENAME"
+		mv $HOME_SRT/RECEIVE/$FILENAME $HOME_SRT/DONE$FILENAME
+		tar -zxvf $HOME_SRT/DONE/$FILENAME
+		echo " file moved to DONE and uncompressed"
+	
 	else
 		echo "XXXXX FAILED XXXXX"
 	fi
